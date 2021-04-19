a5373512c0df5fa0d94a524fe4d814456156a40e
MIZU <QWPO7854@GMAIL.COM>
2021-04-20 02:47:51 +0800

    creat viewmodel

---
+++ Account/Controllers/CreatAccountController.cs
@@ -0,0 +1,20 @@
+﻿using Account.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Account.Controllers {
+    public class CreatAccountController : Controller {
+        // GET: CreatAccount
+        public ActionResult Index() {
+            return View(new AccountData());
+        }
+
+        [HttpPost]
+        public ActionResult Index(AccountData data) {
+            return View(data);
+        }
+    }
+}
---
+++ Account/ViewModels/AccountData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Account.ViewModels {
+    public class AccountData {
+        public string Account {get; set;}
+        public string Password {get; set;}
+    }
+}
